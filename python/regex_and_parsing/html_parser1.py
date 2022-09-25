d3caeb25c1c3959d1e2d5272a9d17dcbb1c3946b
Alfonso Montilla <montilla05alfonso@gmail.com>
2022-09-25 19:34:23 -0300

    add html parsing part 1

---
+++ python/regex_and_parsing/html_parser1.py
@@ -0,0 +1,53 @@
+from html.parser import HTMLParser
+from typing import Optional, Tuple
+
+# create a subclass and override the handler methods
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag: str, attrs: list[Tuple[str, Optional[str]]]) -> None:
+        print(f"Start : {tag}")
+        self._print_attrs(attrs)
+
+    def handle_endtag(self, tag: str) -> None:
+        print(f"End   : {tag}")
+
+    def handle_startendtag(self, tag: str, attrs: list[Tuple[str, Optional[str]]]) -> None:
+        print(f"Empty : {tag}")
+        self._print_attrs(attrs)
+
+    def _print_attrs(self, attrs: list[Tuple[str, Optional[str]]]) -> None:
+        for attr_name, attr_value in attrs:
+            print(f"-> {attr_name} > {attr_value if attr_value else 'None'}")
+
+
+class Reader():
+    MAX_LINES = 100
+
+    def readlines(self) -> list[str]:
+        number_of_lines = int(input())
+        if number_of_lines > self.MAX_LINES:
+            raise ValueError("Number of lines is too big")
+        lines = []
+        for _ in range(number_of_lines):
+            lines.append(input())
+        return lines
+
+
+# instantiate the parser and fed it some HTML
+
+
+def main() -> None:
+    reader = Reader()
+    html_content = reader.readlines()
+    parser = MyHTMLParser()
+    parser.feed("".join(html_content))
+
+    # parser.feed("<html><head><title>HTML Parser - I</title></head>" +
+    #             "<body data-modal-target class='1'><h1>HackerRank</h1><br /></body></html>")
+    # parser.feed("<html><head><title>HTML Parser - I</title></head>"
+    #             + "<body><h1>HackerRank</h1><br /></body></html>")
+
+
+if __name__ == "__main__":
+    main()
