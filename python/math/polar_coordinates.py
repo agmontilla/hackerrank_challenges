d12be34fdf66b94177b8acc6858e46422c1be930
Alfonso Montilla <montilla05alfonso@gmail.com>
2023-06-10 12:06:47 -0300

    feat: add polar coodinates challenge

---
+++ python/math/polar_coordinates.py
@@ -0,0 +1,23 @@
+from cmath import phase
+from math import sqrt
+
+
+class PolarCoordinates():
+    """ Polar Coordinates Class """
+
+    def __init__(self, complex_number: str) -> None:
+        self.complex_number = complex(complex_number)
+
+    def __str__(self) -> str:
+        """ Returns the polar coordinates of a complex number """
+        return f'{sqrt(self.complex_number.real**2+self.complex_number.imag**2)}\n{phase(self.complex_number)}'
+
+
+def main() -> None:
+    complex_number = input()
+    polar_coordinates = PolarCoordinates(complex_number)
+    print(polar_coordinates)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/math/test_polar_coodinates.py
@@ -0,0 +1,12 @@
+from python.math.polar_coordinates import PolarCoordinates
+
+
+class TestPolarCoordinates:
+    """ Test Class for Polar Coordinates Class """
+
+    def test_str_representation_is_working(self) -> None:
+        """ Test __str__ representation """
+        complex_number = '1+2j'
+        expected = '2.23606797749979\n1.1071487177940904'
+        polar_coordinates = PolarCoordinates(complex_number)
+        assert str(polar_coordinates) == expected
