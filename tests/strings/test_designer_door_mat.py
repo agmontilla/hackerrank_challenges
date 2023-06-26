85f0b6e7a48813f6b777b79cb6c31c213fdcaf15
Alfonso Montilla <montilla05alfonso@gmail.com>
2023-06-25 21:47:43 -0300

    test: add test

---
+++ tests/strings/test_designer_door_mat.py
@@ -0,0 +1,39 @@
+
+import pytest
+from pytest import CaptureFixture
+
+from python.strings.designer_door_mat import check_columns, is_odd, print_dot
+
+
+class TestDesignerDoorMat():
+
+    def test_designer_door_mat_is_working(self, capfd: CaptureFixture) -> None:
+        """ Test if designer_door_mat is working as expected """
+        expected = (
+            "------------.|.------------\n"
+            "---------.|..|..|.---------\n"
+            "------.|..|..|..|..|.------\n"
+            "---.|..|..|..|..|..|..|.---\n"
+            "----------WELCOME----------\n"
+            "---.|..|..|..|..|..|..|.---\n"
+            "------.|..|..|..|..|.------\n"
+            "---------.|..|..|.---------\n"
+            "------------.|.------------\n"
+        )
+
+        print_dot(9, 27)
+
+        out, _ = capfd.readouterr()
+        assert out == expected
+
+    def test_is_odd_is_working(self) -> None:
+        """ Test if is_odd is working as expected """
+        assert is_odd(3)
+        with pytest.raises(ValueError):
+            is_odd(4)
+
+    def test_check_columns_is_working(self) -> None:
+        """ Test if check_columns is working as expected """
+        assert check_columns(3, 9)
+        with pytest.raises(ValueError):
+            check_columns(3, 10)
