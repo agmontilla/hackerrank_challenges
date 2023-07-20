d9717c9390c8c0c10b5c1aac1f6ad4de5308e44a
Alfonso Montilla <montilla05alfonso@gmail.com>
2023-07-20 20:27:05 -0300

    refactor: change script to create a function

--- python/sets/add.py
+++ python/sets/add.py
@@ -1,7 +1,14 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-
-if __name__ == '__main__':
-    countries_number = int(input(''))
+def add(countries_number: int) -> None:
     countries = {input('') for _ in range(countries_number)}
     print(countries.__len__())
+
+
+def main() -> None:
+    countries_number = int(input(''))
+    add(countries_number)
+
+
+if __name__ == '__main__':
+    main()
