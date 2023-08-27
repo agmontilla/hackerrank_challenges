9f4bd87491c73198d7037c36705cfcf04f9a5a08
Alfonso Montilla <montilla05alfonso@gmail.com>
2023-08-27 16:24:25 -0300

    refactor: update captains room challenge

--- python/sets/captains_room.py
+++ python/sets/captains_room.py
@@ -1,11 +1,24 @@
 from collections import Counter
+from typing import List
+
+
+class CaptainRoom:
+
+    def __init__(self, room_numbers: List[int]) -> None:
+        self.room_numbers = room_numbers
+
+    def get_captain_room_number(self) -> int:
+        captain_number = Counter(self.room_numbers).most_common()[-1][0]
+        return int(captain_number)
+
+
+def main() -> None:
+    _ = int(input())
+    room_numbers = sorted(list(map(int, input().split())))
+
+    captain_room = CaptainRoom(room_numbers)
+    print(captain_room.get_captain_room_number())
+
 
 if __name__ == "__main__":
-
-    n = int(input())
-    room_numbers = sorted(list(map(int, input().split())))
-    uniques = set(room_numbers)
-
-    captain_number = Counter(room_numbers).most_common()[-1][0]
-
-    print(f'{captain_number}')
+    main()
