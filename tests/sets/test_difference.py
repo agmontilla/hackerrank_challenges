73cbb01b682ca8f4573e5dec0ea11eaa0078c838
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-02-20 12:34:51 -0300

    test: add new test

--- tests/sets/test_difference.py
+++ tests/sets/test_difference.py
@@ -1,6 +1,7 @@
 """ Test difference of two sets """
+from pytest import CaptureFixture, MonkeyPatch
 
-from python.sets.difference import run_difference
+from python.sets.difference import main, run_difference
 
 
 class TestDifference:
@@ -11,3 +12,16 @@ class TestDifference:
         set_a = {1, 2, 3, 4, 5, 6, 7, 8, 9}
         set_b = {10, 1, 2, 3, 11, 21, 55, 6, 8}
         assert run_difference(set_a, set_b) == 4
+
+    def test_print_difference_operation(self, capsys: CaptureFixture, monkeypatch: MonkeyPatch) -> None:
+        """ print difference operation is working as expected """
+        inputs = [
+            "9",
+            "1 2 3 4 5 6 7 8 9",
+            "9",
+            "10 1 2 3 11 21 55 6 8"
+        ]
+        monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+        main()
+        captured = capsys.readouterr()
+        assert captured.out == "4\n"
