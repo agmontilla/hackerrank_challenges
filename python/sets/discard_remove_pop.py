4116aa77983706dd770d765c3524018b4bfd01ca
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-02-20 13:03:17 -0300

    test: add test to check discard, remove and pop operations

--- python/sets/discard_remove_pop.py
+++ python/sets/discard_remove_pop.py
@@ -1,14 +1,25 @@
 """ Discard, Remove, Pop """
+from typing import Set
 
-if __name__ == "__main__":
 
-    n = int(input())
-    s = set(map(int, input().split()))
-    operations_number = int(input())
-
-    while operations_number > 0:
+def run_discard_remove_pop(s: Set, operations: int) -> Set:
+    """ Execute operations """
+    while operations > 0:
         func, *args = input().split()
         getattr(s, func)(*map(int, args))
-        operations_number -= 1
+        operations -= 1
 
-    print(f'{sum(s)}')
+    return s
+
+
+def main() -> None:
+    """ Main method """
+    n = int(input())
+    s = set(map(int, input().split()[:n]))
+    operations_number = int(input())
+
+    print(f'{sum(run_discard_remove_pop(s, operations_number))}')
+
+
+if __name__ == "__main__":
+    main()
