8dd9a8132c31a0124a2a67a129102f0cb08c2dca
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-02-20 12:13:48 -0300

    test: add test to check subset

--- python/sets/check_subset.py
+++ python/sets/check_subset.py
@@ -1,8 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 """ Check Subset """
 
-if __name__ == "__main__":
 
+def check_subset() -> None:
+    """ Check if the first set is a subset of the second set """
     test_cases = int(input())
 
     sets = [(int(input()), set(input().split()))
@@ -10,3 +11,12 @@ if __name__ == "__main__":
 
     for i in range(0, 2*test_cases, 2):
         print(sets[i][1].issubset(sets[i+1][1]))
+
+
+def main() -> None:
+    """ Main function """
+    check_subset()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/sets/test_subset.py
@@ -0,0 +1,19 @@
+""" Tests for the check_subset function """
+
+from pytest import CaptureFixture, MonkeyPatch
+
+from python.sets.check_subset import check_subset
+
+
+class TestSubset():
+    """ Test cases for check_subset function """
+
+    def test_check_subset(self, capsys: CaptureFixture, monkeypatch: MonkeyPatch) -> None:
+        """ check_subset is working as expected """
+        inputs = ["3", "5", "1 2 3 5 6", "9",
+                  "9 8 5 6 3 2 1 4 7", "1", "2", "5", "3 6 5 4 1",
+                  "7", "1 2 3 5 6 8 9", "3", "9 8 2"]
+        monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+        check_subset()
+        captured = capsys.readouterr()
+        assert captured.out == "True\nFalse\nFalse\n"
