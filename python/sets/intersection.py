48ada4e9117429f3853fd02eb6c4bcdd538b99da
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-02-20 13:18:03 -0300

    test: add test to check set intersection

--- python/sets/intersection.py
+++ python/sets/intersection.py
@@ -1,13 +1,25 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 """ Set .intersection() Operation """
+from typing import Set
 
-if __name__ == "__main__":
 
+def run_intersection(set_a: Set, set_b: Set) -> int:
+    """ Run set intersection function """
+    total = len(set_a.intersection(set_b))
+    return total
+
+
+def main() -> None:
+    """ Main function """
     _ = int(input())
     english_students = set(map(int, input().split()))
     _ = int(input())
     french_students = set(map(int, input().split()))
 
-    students_total = len(english_students.intersection(french_students))
+    students_total = run_intersection(english_students, french_students)
 
     print(f'{students_total}')
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/sets/test_intersection.py
@@ -0,0 +1,28 @@
+""" """
+
+from pytest import CaptureFixture, MonkeyPatch
+
+from python.sets.intersection import main, run_intersection
+
+
+class TestIntersection:
+    """ Test cases for intersection function """
+
+    def test_intersection_operation_is_working_as_expected(self) -> None:
+        """ intersection operation is working as expected """
+        set_a = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        set_b = {10, 1, 2, 3, 11, 21, 55, 6, 8}
+        assert run_intersection(set_a, set_b) == 5
+
+    def test_print_intersection_operation(self, capsys: CaptureFixture, monkeypatch: MonkeyPatch) -> None:
+        """ print intersection operation is working as expected """
+        inputs = [
+            "9",
+            "1 2 3 4 5 6 7 8 9",
+            "9",
+            "10 1 2 3 11 21 55 6 8"
+        ]
+        monkeypatch.setattr('builtins.input', lambda: inputs.pop(0))
+        main()
+        captured = capsys.readouterr()
+        assert captured.out == "5\n"
