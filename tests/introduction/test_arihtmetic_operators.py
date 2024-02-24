4188e9734fdafe2e2f134192522b8f3f705386c6
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-02-24 17:19:14 -0300

    test: add new test

--- tests/introduction/test_arihtmetic_operators.py
+++ tests/introduction/test_arihtmetic_operators.py
@@ -1,8 +1,11 @@
 """ Test cases for ArithmeticOperators"""
 
-from pytest import CaptureFixture
+from pytest import CaptureFixture, MonkeyPatch
 
-from challenges.introduction.arithmetic_operators import show_arithmetic_operations
+from challenges.introduction.arithmetic_operators import (
+    main,
+    show_arithmetic_operations,
+)
 
 
 class TestArithmeticOperators:
@@ -19,3 +22,17 @@ class TestArithmeticOperators:
 
         # Assert
         assert captured.out == expected_output
+
+    def test_show_arithmetic_operations_main_program_is_working(self, capsys: CaptureFixture, monkeypatch: MonkeyPatch) -> None:
+        """Show arithmetic operations main program is working"""
+        # Arrange
+        input_values = ["3", "2"]
+        monkeypatch.setattr("builtins.input", lambda: input_values.pop(0))
+        expected_output = "5\n1\n6\n"
+
+        # Act
+        main()
+
+        # Assert
+        out, _ = capsys.readouterr()
+        assert out == expected_output
