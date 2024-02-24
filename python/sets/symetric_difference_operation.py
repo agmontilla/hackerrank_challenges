455371dd3fd301caad07512ccfae21bc25da10fd
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-02-24 11:12:01 -0300

    test: add check to set syemtric difference operation

--- python/sets/symetric_difference_operation.py
+++ python/sets/symetric_difference_operation.py
@@ -2,14 +2,24 @@
 """ Set .symmetric_difference() Operation """
 # pylint: disable=duplicate-code
 
-if __name__ == "__main__":
 
+def get_symetric_difference(set_a: set, set_b: set) -> int:
+    """Get the symmetric difference"""
+    total = len(set_a.symmetric_difference(set_b))
+    return total
+
+
+def main() -> None:
+    """Main function"""
     _ = int(input())
     english_students = set(map(int, input().split()))
     _ = int(input())
     french_students = set(map(int, input().split()))
 
-    students_total = len(
-        english_students.symmetric_difference(french_students))
+    students_total = get_symetric_difference(english_students, french_students)
 
-    print(f'{students_total}')
+    print(f"{students_total}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/sets/test_symetric_difference_operation.py
@@ -0,0 +1,29 @@
+""" Test Symetric Difference Operation """
+
+from pytest import CaptureFixture, MonkeyPatch
+
+from python.sets.symetric_difference_operation import get_symetric_difference, main
+
+
+class TestSymetricDifferenceOperation:
+    """Test Symetric Difference Operation"""
+
+    def test_symetric_difference_operation_is_working(self) -> None:
+        """Symetric difference operation is working as expected"""
+        english_students = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        french_students = {10, 1, 2, 3, 11, 21, 55, 6, 8}
+
+        result = get_symetric_difference(english_students, french_students)
+
+        assert result == 8
+
+    def test_symetric_difference_main_program_is_working(self, capfd: CaptureFixture, monkeypatch: MonkeyPatch) -> None:
+        """Symetric difference main program is working as expected"""
+
+        inputs = ["9", "1 2 3 4 5 6 7 8 9", "9", "10 1 2 3 11 21 55 6 8"]
+        monkeypatch.setattr("builtins.input", lambda: inputs.pop(0))
+
+        main()
+
+        out, _ = capfd.readouterr()
+        assert out == "8\n"
