1354e635dd7e57916a611c3145f4ea17d55abbfe
Alfonso Montilla <alfonso.montilla@tarmac.io>
2024-03-29 00:25:15 -0300

    fix: changes in legacy mode printing

--- challenges/numpy/mean_var_std.py
+++ challenges/numpy/mean_var_std.py
@@ -1,30 +1,13 @@
 """ Mean, Var, and Std Challenge """
-from dataclasses import dataclass
-from typing import Union
-
 import numpy as np
 
 from challenges.numpy.utils import get_entries
 
-CustomNdArray = Union[float, np.ndarray, int]
 
-
-@dataclass
-class NumpyData:
-    """Dataclass for mean, var, and std"""
-
-    mean: CustomNdArray
-    var: CustomNdArray
-    std: CustomNdArray
-
-    def __str__(self) -> str:
-        return f"{self.mean}\n{self.var}\n{self.std}"
-
-
-def mean_var_std(data: list[list[int]]) -> NumpyData:
+def mean_var_std(data: list[list[int]]) -> tuple:
     """Appliyng min and max operations on data"""
     arr = np.array(data)
-    return NumpyData(
+    return (
         arr.mean(axis=1),
         arr.var(axis=0),
         np.round(arr.std(), 11),
@@ -33,10 +16,8 @@ def mean_var_std(data: list[list[int]]) -> NumpyData:
 
 def main() -> None:
     """Main function"""
-    # np.set_printoptions(legacy=False)
-
-    result = mean_var_std(get_entries())
-    print(result)
+    np.set_printoptions(legacy="1.13")
+    print(*mean_var_std(get_entries()), sep="\n")
 
 
 if __name__ == "__main__":
--- tests/numpy/test_mean_var_std.py
+++ tests/numpy/test_mean_var_std.py
@@ -11,16 +11,16 @@ class TestMeanVarStd:
     def test_mean_var_std(self) -> None:
         """Test mean_var_std function"""
         data = [[1, 2], [3, 4]]
-        result = mean_var_std(data)
-        assert all(np.equal(result.mean, np.array([1.5, 3.5])))
-        assert all(np.equal(result.var, np.array([1.0, 1.0])))
-        assert result.std == 1.11803398875
+        mean, var, std = mean_var_std(data)
+        assert all(np.equal(mean, np.array([1.5, 3.5])))
+        assert all(np.equal(var, np.array([1.0, 1.0])))
+        assert std == 1.11803398875
 
     def test_main(self, capsys: CaptureFixture, monkeypatch: MonkeyPatch) -> None:
         """Test main function"""
         input_values = ["2 2", "1 2", "3 4"]
         monkeypatch.setattr("builtins.input", lambda: input_values.pop(0))
-        expected_output = "[1.5 3.5]\n[1. 1.]\n1.11803398875\n"
+        expected_output = "[ 1.5  3.5]\n[ 1.  1.]\n1.11803398875\n"
         main()
         captured = capsys.readouterr()
         assert captured.out == expected_output
